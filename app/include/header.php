ef447854cb122b8be27692553930a3efa675cc3c
spirits <florian.schaufelberger@hotmail.com>
2015-09-25 05:23:18 +0200

    Add du footer et Header

---
+++ app/include/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Classement Players CSGO</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper" class="wrapper">
+					<div id="header">
+
+						<!-- Logo -->
+							<div id="logo">
+								<h1><a href="index.html">CSGO Players Ranks</a></h1>
+								<p></p>
+							</div>
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li class="current"><a href="index.html">Home</a></li>
+									<!--<li>
+										<a href="#">Dropdown</a>
+										<ul>
+											<li><a href="#">Lorem ipsum</a></li>
+											<li><a href="#">Magna veroeros</a></li>
+											<li><a href="#">Etiam nisl</a></li>
+											<li>
+												<a href="#">Sed consequat</a>
+												<ul>
+													<li><a href="#">Lorem dolor</a></li>
+													<li><a href="#">Amet consequat</a></li>
+													<li><a href="#">Magna phasellus</a></li>
+													<li><a href="#">Etiam nisl</a></li>
+													<li><a href="#">Sed feugiat</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Nisl tempus</a></li>
+										</ul>
+									</li>-->
+									<li><a href="left-sidebar.html">Membres</a></li>
+									<li><a href="right-sidebar.html">Se Connecter</a></li>
+									<li><a href="no-sidebar.html">S'inscrire</a></li>
+								</ul>
+							</nav>
+
+					</div>
+				</div>
