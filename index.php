5ac966ac5282394679fa31561d5b2cadb3e792f8
spirits <florian.schaufelberger@hotmail.com>
2015-09-25 07:45:59 +0200

    Bonne indentation des fichiers

--- index.php
+++ index.php
@@ -4,28 +4,29 @@
 <html lang="fr">
 
 <head>
-<!-- META -->
-	<meta charset="UTF-8"/>	<meta name="Generator" content="XpressEngine" />
-<!-- TITLE -->
-	<title>Classement Players CSGO</title>
-<!-- CSS -->
-<link rel="stylesheet" href="include/css/style.css" media="all" />
-<!-- JS -->
+    <!-- META -->
+    <meta charset="UTF-8"/>
+    <meta name="Generator" content="XpressEngine"/>
+    <!-- TITLE -->
+    <title>Classement Players CSGO</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="include/css/style.css" media="all"/>
+    <!-- JS -->
 </head>
 <body>
 <div id="header">
-	<div class="inner headerTop">
+    <div class="inner headerTop">
         <div class="language_A">
-		<h2 class="ltit"><a href="#">LANGUAGE : <span style="color:#CCC;">Français</span></a></h2>
-			<ul class="list">            
-                <li><a onclick="doChangeLangType('en');return false;" href="#">Anglais</a>                        </li>
+            <h2 class="ltit"><a href="#">LANGUAGE : <span style="color:#CCC;">Français</span></a></h2>
+            <ul class="list">
+                <li><a onclick="doChangeLangType('en');return false;" href="#">Anglais</a></li>
             </ul>
-			</div>
-        	<ul class="util" >
-				<li class="loginbt"><a href="#">Se Connecter</a></li>
-				<li><a href="#">S'inscrire</a></li>
-            </ul>
-</div>
+        </div>
+        <ul class="util">
+            <li class="loginbt"><a href="#">Se Connecter</a></li>
+            <li><a href="#">S'inscrire</a></li>
+        </ul>
+    </div>
 
 </body>
 </html>
