b1c09547942cfd4abf296b48e9c27ca064e25599
spirits <florian.schaufelberger@hotmail.com>
2015-09-25 06:55:12 +0200

    Modification de gitIgnore pour facilement utiliser npm sans push des fichiers inutiles

--- app/index.php
+++ app/index.php
@@ -1,131 +1,148 @@
 <?php
-include ('include/header.php') ;
+include('include/header.php');
 ?>
 
-			<!-- Intro -->
-				<div id="footer-wrapper" class="wrapper style1">
-					<div class="title">The Introduction</div>
-					<section id="intro" class="container">
-						<p class="style1">So in case you were wondering what this is all about ...</p>
-						<p class="style2">
-								Escape Velocity is a free responsive<br class="mobile-hide" />
-							site template by <a href="http://html5up.net" class="nobr">HTML5 UP</a>
-						</p>
-						<p class="style3">It's <strong>responsive</strong>, built on <strong>HTML5</strong> and <strong>CSS3</strong>, and released for
-						free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0 license</a>, so use it for any of
-						your personal or commercial projects &ndash; just be sure to credit us!</p>
-						<ul class="actions">
-						<li><a href="#" class="button style3 big">Proceed</a></li>
-						</ul>
-					</section>
-				</div>
+<!-- Intro -->
+<div id="footer-wrapper" class="wrapper style1">
+    <div class="title">Introduction</div>
+    <section id="intro" class="container">
+        <p class="style1">Bienvenue sur Csgo Ranks</p>
 
-			<!-- Main -->
-				<div class="wrapper">
-					<div class="title">Faites Vous Connaﵲe</div>
-					<div id="main" class="container">
+        <p class="style2">
+            Se site a pour but que vous puissiez facilement créer des équipes en cherchant quel type de joueur vous
+            désirez, quel type de joueur dont vous avez besoin.
+        </p>
+    </section>
+</div>
 
-						<!-- Image -->
-							<a href="#" class="image featured">
-								<img src="images/pic01.jpg" alt="" />
-							</a>
+<!-- Main -->
+<div class="wrapper">
+    <div class="title">Faites Vous Connaﵲe</div>
+    <div id="main" class="container">
 
-						<!-- Features -->
-							<section id="features">
-								<header class="style1">
-									<h2>Dolor consequat feugiat amet veroeros</h2>
-									<p>Feugiat dolor nullam orci pretium phasellus justo</p>
-								</header>
-								<div class="feature-list">
-									<div class="row">
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-comment">Mattis velit diam vulputate</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-refresh">Lorem ipsum dolor sit veroeros</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-									<div class="row">
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-picture-o">Pretium phasellus justo lorem</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-cog">Tempus sed pretium orci</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-									<div class="row">
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-wrench">Aliquam consequat et feugiat</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="6u 12u(mobile)">
-											<section>
-												<h3 class="icon fa-check">Dolore laoreet aliquam mattis</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-								</div>
-								<ul class="actions actions-centered">
-									<li><a href="#" class="button style1 big">Get Started</a></li>
-									<li><a href="#" class="button style2 big">More Info</a></li>
-								</ul>
-							</section>
+        <!-- Image -->
+        <a href="#" class="image featured">
+            <img src="images/pic01.jpg" alt=""/>
+        </a>
 
-					</div>
-				</div>
+        <!-- Features -->
+        <section id="features">
+            <header class="style1">
+                <h2>Dolor consequat feugiat amet veroeros</h2>
 
-			<!-- Highlights -->
-				<div id="footer-wrapper" class="wrapper">
-					<div class="title">The Endorsements</div>
-					<div id="highlights" class="container">
-						<div class="row 150%">
-							<div class="4u 12u(mobile)">
-								<section class="highlight">
-									<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-									<h3><a href="#">Aliquam diam consequat</a></h3>
-									<p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-									<ul class="actions">
-										<li><a href="#" class="button style1">Learn More</a></li>
-									</ul>
-								</section>
-							</div>
-							<div class="4u 12u(mobile)">
-								<section class="highlight">
-									<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-									<h3><a href="#">Nisl adipiscing sed lorem</a></h3>
-									<p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-									<ul class="actions">
-										<li><a href="#" class="button style1">Learn More</a></li>
-									</ul>
-								</section>
-							</div>
-							<div class="4u 12u(mobile)">
-								<section class="highlight">
-									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-									<h3><a href="#">Mattis tempus lorem</a></h3>
-									<p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-									<ul class="actions">
-										<li><a href="#" class="button style1">Learn More</a></li>
-									</ul>
-								</section>
-							</div>
-						</div>
-					</div>
-<?php
-include ('include/footer.php');
-?>
+                <p>Feugiat dolor nullam orci pretium phasellus justo</p>
+            </header>
+            <div class="feature-list">
+                <div class="row">
+                    <div class="6u 12u(mobile)">
+                        <section>
+                            <h3 class="icon fa-comment">Mattis velit diam vulputate</h3>
+
+                            <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget
+                                mattis at, laoreet vel velit lorem.</p>
+                        </section>
+                    </div>
+                    <div class="6u 12u(mobile)">
+                        <section>
+                            <h3 class="icon fa-refresh">Lorem ipsum dolor sit veroeros</h3>
+
+                            <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget
+                                mattis at, laoreet vel velit lorem.</p>
+                        </section>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="6u 12u(mobile)">
+                        <section>
+                            <h3 class="icon fa-picture-o">Pretium phasellus justo lorem</h3>
+
+                            <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget
+                                mattis at, laoreet vel velit lorem.</p>
+                        </section>
+                    </div>
+                    <div class="6u 12u(mobile)">
+                        <section>
+                            <h3 class="icon fa-cog">Tempus sed pretium orci</h3>
+
+                            <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget
+                                mattis at, laoreet vel velit lorem.</p>
+                        </section>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="6u 12u(mobile)">
+                        <section>
+                            <h3 class="icon fa-wrench">Aliquam consequat et feugiat</h3>
+
+                            <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget
+                                mattis at, laoreet vel velit lorem.</p>
+                        </section>
+                    </div>
+                    <div class="6u 12u(mobile)">
+                        <section>
+                            <h3 class="icon fa-check">Dolore laoreet aliquam mattis</h3>
+
+                            <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget
+                                mattis at, laoreet vel velit lorem.</p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <ul class="actions actions-centered">
+                <li><a href="#" class="button style1 big">Get Started</a></li>
+                <li><a href="#" class="button style2 big">More Info</a></li>
+            </ul>
+        </section>
+
+    </div>
+</div>
+
+<!-- Highlights -->
+<div id="footer-wrapper" class="wrapper">
+    <div class="title">The Endorsements</div>
+    <div id="highlights" class="container">
+        <div class="row 150%">
+            <div class="4u 12u(mobile)">
+                <section class="highlight">
+                    <a href="#" class="image featured"><img src="images/pic02.jpg" alt=""/></a>
+
+                    <h3><a href="#">Aliquam diam consequat</a></h3>
+
+                    <p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate
+                        mattis amet laoreet lorem.</p>
+                    <ul class="actions">
+                        <li><a href="#" class="button style1">Learn More</a></li>
+                    </ul>
+                </section>
+            </div>
+            <div class="4u 12u(mobile)">
+                <section class="highlight">
+                    <a href="#" class="image featured"><img src="images/pic03.jpg" alt=""/></a>
+
+                    <h3><a href="#">Nisl adipiscing sed lorem</a></h3>
+
+                    <p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate
+                        mattis amet laoreet lorem.</p>
+                    <ul class="actions">
+                        <li><a href="#" class="button style1">Learn More</a></li>
+                    </ul>
+                </section>
+            </div>
+            <div class="4u 12u(mobile)">
+                <section class="highlight">
+                    <a href="#" class="image featured"><img src="images/pic04.jpg" alt=""/></a>
+
+                    <h3><a href="#">Mattis tempus lorem</a></h3>
+
+                    <p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate
+                        mattis amet laoreet lorem.</p>
+                    <ul class="actions">
+                        <li><a href="#" class="button style1">Learn More</a></li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+    </div>
+    <?php
+    include('include/footer.php');
+    ?>
