6a22feb32783eb1c04af39de11307ce6112d7584
Arkadi <deadkillergfx@gmail.com>
2015-09-25 04:26:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,22 @@
 <!-- TITLE -->
 	<title>Classement Players CSGO</title>
 <!-- CSS -->
-
+<link rel="stylesheet" href="include/css/style.css" media="all" />
 <!-- JS -->
 </head>
 <body>
+<div id="header">
+	<div class="inner headerTop">
+        <div class="language_A">
+		<h2 class="ltit"><a href="#">LANGUAGE : <span style="color:#CCC;">Français</span></a></h2>
+			<ul class="list">            
+                <li><a onclick="doChangeLangType('en');return false;" href="#">Anglais</a>                        </li>
+            </ul>
+			</div>
+        	<ul class="util" >
+				<li class="loginbt"><a href="#">Se Connecter</a></li>
+				<li><a href="#">S'inscrire</a></li>
+            </ul>
+</div>
 </body>
 </html>
