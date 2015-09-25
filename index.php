457d0bd4dbeee5682d97a345ce55bc18937fc137
Arkadi <deadkillergfx@gmail.com>
2015-09-25 04:13:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="fr">
+
 <head>
+<!-- META -->
+	<meta charset="UTF-8"/>	<meta name="Generator" content="XpressEngine" />
+<!-- TITLE -->
+	<title>Phoenix-eSport MULTIGAMING</title>
+<!-- CSS -->
+
+<!-- JS -->
 </head>
 <body>
 </body>
